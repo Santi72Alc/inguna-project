d05e84d5ae631a7a74ad5d62d47b7e2f1ac886a2
Santiago San Roman Jativa <santi72alc@gmail.com>
2021-04-12 01:21:39 +0200

    go back to start with craco & try

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,39 @@
-// tailwind.config.js
-
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-    purge: [
-        './src/**/*.{js,jsx,ts,tsx}', 
-        './public/index.html'
-    ],
-    theme: {
-        colors: {
-            transparent: 'transparent',
-            current: 'currentColor',
-            amber: colors.amber,
-            black: colors.black,
-            blue: colors.blue,
-            emerald: colors.emerald,
-            gray: colors.gray,
-            indigo: colors.indigo,
-            red: colors.red,
-            yellow: colors.yellow,
-            white: colors.white,
-        },
-        screens: {
-            'sm': {'max': '480px'},
-            'md': {'min': '481px'},
-            'lg': {'min': '765px'},
-            'xl': {'min': '1280px'},
-            '2xl': {'min': '1366'},
-            '3xl': {'min': '1440px'},
-          },
+  purge: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      amber: colors.amber,
+      black: colors.black,
+      blue: colors.blue,
+      emerald: colors.emerald,
+      gray: colors.gray,
+      indigo: colors.indigo,
+      red: colors.red,
+      yellow: colors.yellow,
+      white: colors.white,
     },
-    variants: {
-        extends: {
-            fontstyle: ['hover']
-        }
+    screens: {
+      'sm': { 'max': '480px' },
+      'md': { 'min': '481px' },
+      'lg': { 'min': '765px' },
+      'xl': { 'min': '1280px' },
+      '2xl': { 'min': '1366' },
+      '3xl': { 'min': '1440px' },
+    },
+    extend: {},
+  },
+  variants: {
+    extends: {
+      fontstyle: ['hover']
     }
+  },
+  plugins: [],
 };
